c2c01e78e27c8ebcfdfd95c2306587f72b24a90b
luzialeticia <leticialuzia@tutamail.com>
2020-08-17 11:13:35 -0300

    Declarando as variáveis, criando o código do if else e finalizando o exercício

--- condicionais/ex2.js
+++ condicionais/ex2.js
@@ -1,2 +1,40 @@
 /* Faça um algoritmo que leia o nome, o sexo e o estado civil de uma pessoa. Caso sexo seja “F” e
 estado civil seja “CASADA”, solicitar o tempo de casada (anos). */
+
+const input = require('readline-sync')
+
+const nome = input.question('Nome: ')
+    console.log(nome)
+
+const sexo = input.question('Gênero (F para feminino, M para masculino, ou NB para Não-binárie): ')
+    if (sexo === "F") {
+        console.log("Feminino")
+    }else if (sexo === "M") {
+        console.log("Masculino")
+    }else if (sexo === "NB") {
+        console.log("Não-binárie")
+    }else {
+        console.log("Digite uma opção válida")
+    }
+
+const estadoCivil = input.question('Estado civil (S para solteire, C para casade, V para viúve, D para divorciade, O para outro): ')
+   if (estadoCivil === "S") {
+       console.log("Solteire")
+   }else if (estadoCivil === "C") {
+        console.log("Casade")
+   }else if (estadoCivil === "V") {
+       console.log("Viúve")
+   }else if (estadoCivil === "D") {
+    console.log("Divorciade")
+   }else if (estadoCivil === "O") {
+    console.log("Outro")
+   }else {
+    console.log("Digite uma opção válida")
+   }
+
+if (sexo === "F" && estadoCivil === "C") {
+    const tempo = parseInt(input.question('Tempo de casada: '))
+    console.log(`${nome} tem ${tempo} anos de casada.`)
+}else {
+    console.log(`${nome} é do gênero ${sexo} e seus estado civil é ${estadoCivil}.`)
+}
