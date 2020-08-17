d95014b937a44cba3c480d5acfa6dedff30d8356
luzialeticia <leticialuzia@tutamail.com>
2020-08-17 10:52:28 -0300

    Criando o código da condicional e finalizando o exercício

--- condicionais/ex1.js
+++ condicionais/ex1.js
@@ -6,3 +6,13 @@ const a = Number(input.question('Digite um valor para A: '))
 const b = Number(input.question('Digite um valor para B: '))
 const c = Number(input.question('Digite um valor para C: '))
 const soma = a + b
+
+console.log(`A = ${a}\nB = ${b}\nC = ${c}\nSoma entre A e B = ${soma}`)
+
+if (soma < c) {
+    console.log(`A soma entre A e B é MENOR que o valor de C.\n${soma} < ${c}`)
+}else if (soma === c) {
+    console.log(`A soma entre A e B é IGUAL ao valor de C.\n${soma} = ${c}`)
+}else {
+    console.log(`A soma entre A e B é MAIOR que o valor de C.\n${soma} > ${c}`)
+}
