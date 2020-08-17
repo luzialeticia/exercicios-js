8b705351e79003d8c56363a70c1ce26a6e3199f3
luzialeticia <leticialuzia@tutamail.com>
2020-08-17 09:27:46 -0300

    Declarando as variáveis e finalizando o exercício

--- operadores-expressoes/ex1.js
+++ operadores-expressoes/ex1.js
@@ -1,4 +1,12 @@
 /* A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um algoritmo para ler as
-dimensões de um terreno e depois exibir a área do terreno. */
+dimensões de um terreno e depois exibir a área do terreno. 
 
+Área do retângulo A = bxh */
 
+const input = require('readline-sync')
+
+const base = parseFloat(input.question('Digite o valor da base do terreno: '))
+const altura = parseFloat(input.question('Digite o valor da altura do terreno: '))
+const area = (base * altura).toFixed(2);
+
+console.log(area);
