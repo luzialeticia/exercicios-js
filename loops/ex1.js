9829a19ebd74387f1227fdb9299e24375cd7a05f
luzialeticia <leticialuzia@tutamail.com>
2020-08-17 14:22:29 -0300

    O código está em loop infinito

--- loops/ex1.js
+++ loops/ex1.js
@@ -1,15 +1,15 @@
 /* Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de
 três e que se encontram no conjunto dos números de 1 até 500. */
 
-let soma = 0
-let i = 1
+var soma = 0
+var i = 1
 
 while (i <= 500) {
     if (i%2!==0 && i%3===0) {
         soma += i
         i++
     }
+    console.log(soma)
 }
 
 //console.log(i)
-console.log(soma)
