18450f1d46d4e07bf6fc03455d68d4ebabf2826e
luzialeticia <leticialuzia@tutamail.com>
2020-08-17 09:33:32 -0300

    Declarando variáveis e finalizando exercício

--- operadores-expressoes/ex2.js
+++ operadores-expressoes/ex2.js
@@ -1,2 +1,9 @@
 /* Faça um algoritmo para calcular quantas ferraduras são necessárias para equipar todos os
 cavalos comprados para um haras. */
+
+const input = require('readline-sync')
+
+const cavalos = parseInt(input.question("Quantos cavalos você possui? "))
+const ferraduras = 4 * cavalos
+
+console.log(`Você vai precisar comprar ${ferraduras} ferraduras.`)
