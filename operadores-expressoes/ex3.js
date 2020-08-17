10b57d4e3aacdc1efd8a345426eafa91999e47a3
luzialeticia <leticialuzia@tutamail.com>
2020-08-17 10:08:18 -0300

    Declarando variáveis e arrumando os cálculos. A procentagem não está saindo correta, verificar

--- operadores-expressoes/ex3.js
+++ operadores-expressoes/ex3.js
@@ -4,3 +4,18 @@ quanto arrecadou com a venda dos pães e broas (juntos), e quanto deve guardar n
 poupança (10% do total arrecadado). Você foi contratado para fazer os cálculos para o dono. Com
 base nestes fatos, faça um algoritmo para ler as quantidades de pães e de broas, e depois calcular
 os dados solicitados. */
+
+const input = require('readline-sync')
+
+const paes = parseInt(input.question('Quantos pães foram vendidos? '))
+const broas = parseInt(input.question('Quantas broas foram vendidas? '))
+
+/*const totalPaes = parseFloat(paes * 0.12).toFixed(2)
+const totalBroas = parseFloat(broas * 1.50).toFixed(2)
+const totalVendidos = parseFloat((totalPaes + totalBroas)).toFixed(2)*/
+
+console.log(`O total arrecadado no dia foi de R$ ${(paes*0.12)+(broas*1.50)}.`)
+
+//const porcentagem = (totalVendidos /10*100).toFixed(2)
+
+console.log(`O valor que você deverá guardar na poupança é R$ ${((paes*0.12)+(broas*1.50)*(10.00/100.00)).toFixed(2)}`)
