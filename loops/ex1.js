d8bcf02f2fac037f9d270d6d3bfffeffa3438267
luzialeticia <leticialuzia@tutamail.com>
2020-08-17 22:02:23 -0300

    Teste, refazendo o loop com while

--- loops/ex1.js
+++ loops/ex1.js
@@ -4,21 +4,20 @@ três e que se encontram no conjunto dos números de 1 até 500. */
 var soma = 0
 var i = 3
 
-/*while (i <= 500) {
+while (i <= 500) {
     if (i%2!==0 && i%3===0) {
         soma += i
-        i++
+        console.log(soma)
     }
-    console.log(soma)
+    i+=3
 }
 
-//console.log(i) */
 
-for (i <= 500; i+3;) {
+/*for (i <= 500; i+3;) {
     if (i%2!==0 && i%3===0) {
         soma = soma + i
         //i+3
         console.log(soma)
     }
-    i+3
-}
+    i+=3
+}*/
