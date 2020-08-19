73e0f950c93efb2401c4d367195db8fc4c004bf8
luzialeticia <leticialuzia@tutamail.com>
2020-08-19 06:49:54 -0300

    Testando um algoritmo com 3 pessoas

--- loops/ex2.js
+++ loops/ex2.js
@@ -2,3 +2,16 @@
 mostrar :
 a. A menor altura do grupo;
 b. A maior altura do grupo; */
+
+const input = require('readline-sync')
+
+let pessoas = 1
+var altura = 0
+
+while (pessoas <= 3) {
+    var altura = parseFloat(input.question("Digite a altura? "))
+    pessoas++
+}
+
+console.log(Math.min(altura))
+console.log(Math.max(altura))
