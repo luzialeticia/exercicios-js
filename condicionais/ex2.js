cd36db77294dfaddca4ec3dfd4f8db450730f0c1
luzialeticia <leticialuzia@tutamail.com>
2020-08-19 06:21:35 -0300

    Trocando const pra let nas variáveis sexo e estadoCivil pra armazenar diferentes strings

--- condicionais/ex2.js
+++ condicionais/ex2.js
@@ -6,43 +6,45 @@ const input = require('readline-sync')
 const nome = input.question('Nome: ')
     console.log(nome)
 
-const sexo = input.question('Gênero (F para feminino, M para masculino, ou NB para Não-binárie): ')
+let sexo = input.question('Gênero (F para feminino, M para masculino, ou NB para Não-binárie): ')
     switch(sexo) {
         case "F":
-            console.log("FEminino");
-            return "Feminino";
-            //break;
-        case "M": 
-            console.log("Masculino");
+            sexo = "Feminino"
+            break;
+        case "M":
+            sexo = "Masculino" 
             break;
         case "NB": 
-            console.log("Não-binárie");
+            sexo = "Não-binárie";
             break;
         default: 
-            console.log("Digite uma opção válida");
+            sexo = "Digite uma opção válida";
     }
+    console.log(sexo);
 
-/*Corrigir
-function estadoCivil () {
-
-    input.question('Estado civil (S para solteire, C para casade, V para viúve, D para divorciade, O para outro): ')
-        switch(estadoCivil) {
-            case "S":
-                return "Solteire";
-            case "C":
-                return "Casade";
-            case "V":
-                return "Viúve";
-            case "D":
-                return "Divorciade";
-            case "O":
-                return "Outro";
-            default:
-                return "Digite uma opção válida";
+let estadoCivil = input.question('Estado civil (S para solteire, C para casade, V para viúve, D para divorciade, O para outro): ')
+    switch(estadoCivil) {
+        case "S":
+            estadoCivil = "Solteire";
+            break;
+        case "C":
+            estadoCivil = "Casade";
+            break;
+        case "V":
+            estadoCivil = "Viúve";
+            break;
+        case "D":
+            estadoCivil = "Divorciade";
+            break;
+        case "O":
+            estadoCivil = "Outro";
+            break
+        default:
+            estadoCivil = "Digite uma opção válida";
         }
-}*/
+        console.log(estadoCivil);
 
-if (sexo === "F" && estadoCivil === "C") {
+if (sexo === "Feminino" && estadoCivil === "Casade") {
     const tempo = parseInt(input.question('Tempo de casada: '))
     console.log(`${nome} tem ${tempo} anos de casada.`)
 }else {
