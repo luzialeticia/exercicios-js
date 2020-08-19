6ebad02d9ab831948b3934ddc40f8b3b519485aa
luzialeticia <leticialuzia@tutamail.com>
2020-08-19 06:31:34 -0300

    Tentando com do while

--- loops/ex1.js
+++ loops/ex1.js
@@ -1,19 +1,29 @@
 /* Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de
 três e que se encontram no conjunto dos números de 1 até 500. */
 
-var soma = 0
-var i = 3
+let soma = 0
+let i = 1
 
-while (i <= 500) {
+do {
+    if (i % 2 !== 0 && i % 3 === 0) {
+        i++
+        soma += i;
+    }
+} while (i <= 500);
+
+console.log(soma);
+
+
+/* while (i <= 500) {
     if (i%2!==0 && i%3===0) {
         soma += i
         console.log(soma)
     }
     i+=3
-}
+} */
 
 
-/*for (i <= 500; i+3;) {
+/* for (i <= 500; i+3;) {
     if (i%2!==0 && i%3===0) {
         soma = soma + i
         //i+3
