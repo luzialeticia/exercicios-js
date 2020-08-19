b9a26530a95d50ac88a0f8c07860e638a2cb41ec
luzialeticia <leticialuzia@tutamail.com>
2020-08-19 04:34:08 -0300

    Outra tentativa de usar função. Corrigir depois

--- condicionais/ex2.js
+++ condicionais/ex2.js
@@ -22,8 +22,9 @@ const sexo = input.question('Gênero (F para feminino, M para masculino, ou NB p
     }
 
 //Ver colocar funções que retornem o estado civil por extenso nas variáveis a seguir:
-/*const estadoCivil = input.question('Estado civil (S para solteire, C para casade, V para viúve, D para divorciade, O para outro): ')
-        switch(str) {
+const estadoCivil = input.question('Estado civil (S para solteire, C para casade, V para viúve, D para divorciade, O para outro): ')
+        function estado(estado) {
+            switch(estado) {
                 case "S":
                     return "Solteire";
                 case "C":
@@ -36,12 +37,12 @@ const sexo = input.question('Gênero (F para feminino, M para masculino, ou NB p
                     return "Outro";
                 default:
                     return "Digite uma opção válida";
-        }
-}*/
+            }
+    }
 
 if (sexo === "F" && estadoCivil === "C") {
     const tempo = parseInt(input.question('Tempo de casada: '))
     console.log(`${nome} tem ${tempo} anos de casada.`)
 }else {
-    console.log(`${nome} é do gênero ${sexo} e seus estado civil é ${estadoCivil}.`)
+    console.log(`${nome} é do gênero ${sexo} e seus estado civil é ${estado}.`)
 }
