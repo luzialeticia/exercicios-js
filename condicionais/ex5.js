11b230031939f43258301ee00e2a4cddd7484449
luzialeticia <leticialuzia@tutamail.com>
2020-08-21 16:14:23 -0300

    Resolvendo o exercício com operador ternário

--- condicionais/ex5.js
+++ condicionais/ex5.js
@@ -1,2 +1,8 @@
 /* Encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo,
 imprimindo o resultado. */
+
+const input = require('readline-sync')
+
+const num = Number(input.question("Digite um nº: "))
+let resposta = num > 0 ? num*2 : num*3
+console.log(resposta)
