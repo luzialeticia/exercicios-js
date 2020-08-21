710181cca150fc96592f5bd44029bbf5f9af9439
luzialeticia <leticialuzia@tutamail.com>
2020-08-21 16:00:40 -0300

    Declarando a variável e criando o algoritmo

--- condicionais/ex3.js
+++ condicionais/ex3.js
@@ -1,2 +1,11 @@
 //Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
 
+const input = require('readline-sync')
+
+const num = Number(input.question('Digite um nº inteiro qualquer: '))
+
+if (num%2 === 0) {
+    console.log(`${num} é par.`)
+}else {
+    console.log(`${num} é ímpar.`)
+}
