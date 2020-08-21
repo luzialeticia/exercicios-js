3bc3429f9b1d695c3df2c79d563c012a09e61c55
luzialeticia <leticialuzia@tutamail.com>
2020-08-21 15:09:04 -0300

    Criando arquivo do ex5 e declarando o varíavel que recebe o readline-syn

---
+++ operadores-expressoes/ex5.js
@@ -0,0 +1,6 @@
+/* Um motorista deseja colocar no seu tanque X reais de gasolina. Escreva um algoritmo para ler o
+preço do litro da gasolina e o valor do pagamento, e exibir quantos litros ele conseguiu colocar no
+tanque.*/
+
+const input = require('readline-sync')
+
