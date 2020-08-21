5636552e55ec0cef76101e83a39b6d5e373d13f1
luzialeticia <leticialuzia@tutamail.com>
2020-08-21 15:04:40 -0300

    Declarando as variáveis

--- operadores-expressoes/ex4.js
+++ operadores-expressoes/ex4.js
@@ -2,3 +2,9 @@
 ela possui. Considere sempre anos completos, e que um ano possui 365 dias. Ex: uma pessoa
 com 19 anos possui 6935 dias de vida; veja um exemplo de saída: MARIA, VOCÊ JÁ VIVEU 6935
 DIAS */
+
+const input = require('readline-sync')
+
+const nome = input.question('Nome :')
+const idade = parseInt(input.question('Idade: '))
+const diasVida = idade * 365
