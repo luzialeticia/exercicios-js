52322a817a7a0ed0f7a60bffd2430b2de559d3b2
luzialeticia <leticialuzia@tutamail.com>
2020-08-23 17:06:18 -0300

    Declarando as variáveis

--- condicionais/ex7.js
+++ condicionais/ex7.js
@@ -1,2 +1,6 @@
 /* Faça um algoritmo que leia uma variável e some 5 caso seja par ou some 8 caso seja ímpar,
 imprimir o resultado desta operação. */
+
+const input = require('readline-sync')
+
+const num = parseInt(input.question("Digite um nº inteiro: "))
