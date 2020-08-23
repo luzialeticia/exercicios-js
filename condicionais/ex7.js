ba957d741a14015a12cc88bc50d1fc3eda1d554f
luzialeticia <leticialuzia@tutamail.com>
2020-08-23 17:11:18 -0300

    Trocando a variável num de const pra let e concluindo o if else

--- condicionais/ex7.js
+++ condicionais/ex7.js
@@ -3,4 +3,11 @@ imprimir o resultado desta operação. */
 
 const input = require('readline-sync')
 
-const num = parseInt(input.question("Digite um nº inteiro: "))
+let num = parseInt(input.question("Digite um nº inteiro: "))
+
+if (num%2===0) {
+    num = num + 5
+} else if (num%2!==0) {
+    num = num + 8
+}
+console.log(num)
