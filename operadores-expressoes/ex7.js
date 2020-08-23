dca189d6afbef03fa1d6231c2ecaf50e1b2f25ac
luzialeticia <leticialuzia@tutamail.com>
2020-08-23 16:50:33 -0300

    Declarando as variáveis

--- operadores-expressoes/ex7.js
+++ operadores-expressoes/ex7.js
@@ -1,2 +1,7 @@
 /* Entrar com o dia e o mês de uma data e informar quantos dias se passaram desde o início do
 ano. Esqueça a questão dos anos bissextos e considere sempre que um mês possui 30 dias. */
+
+const input = require('readline-sync')
+
+const dia = parseInt(input.question("Dia: "))
+const mes = parseInt(input.question("Mês: "))
