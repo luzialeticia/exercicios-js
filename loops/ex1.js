f925cd008ed8774410282f02cd69ebaefd50c2f2
luzialeticia <leticialuzia@tutamail.com>
2020-08-24 10:42:53 -0300

    Declarando a variável que chama o readline-sync

--- loops/ex1.js
+++ loops/ex1.js
@@ -1,3 +1,5 @@
 /* Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de
 três e que se encontram no conjunto dos números de 1 até 500. */
 
+const input = require('readline-sync')
+
