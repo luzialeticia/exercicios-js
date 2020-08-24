8fb5b3a95e3ce1e524720e9c0c96b8622f02b86a
luzialeticia <leticialuzia@tutamail.com>
2020-08-24 11:03:42 -0300

    Declarando a variável do array e criando a lógica que insere itens no array

--- loops/ex2.js
+++ loops/ex2.js
@@ -1,5 +1,14 @@
 /* Desenvolver um algoritmo que leia a altura de 15 pessoas. Este programa deverá calcular e
-mostrar :
+mostrar:
 a. A menor altura do grupo;
 b. A maior altura do grupo; */
 
+const input = require('readline-sync')
+
+let alturas = []
+
+for (let i = 1; i <= 15; i++) {
+    let altura = parseFloat(input.question("Digite a altura: "))
+    alturas.push(altura)
+}
+console.log(alturas)
