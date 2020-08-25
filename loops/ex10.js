d1861d5c71c5312d91f918ad370a5c7e60ab6eb2
luzialeticia <leticialuzia@tutamail.com>
2020-08-25 18:29:02 -0300

    Finalizando o algoritmo

--- loops/ex10.js
+++ loops/ex10.js
@@ -4,8 +4,9 @@ A! e o seu resultado. Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120 */
 const input = require('readline-sync')
 
 let a = parseInt(input.question("Digite um valor inteiro para calcular seu fatorial: "))
+let fatorial = 1
 
 for (let i = 1; i <= a; i++) {
-    let result = i*i
-    console.log(result)
+    fatorial = fatorial * i
 }
+console.log(fatorial)
