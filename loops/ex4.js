8417ff4f854a0042ea64412f5a7af24cad1db9b9
luzialeticia <leticialuzia@tutamail.com>
2020-08-25 20:29:06 -0300

    Tentando com while e if else

--- loops/ex4.js
+++ loops/ex4.js
@@ -3,13 +3,31 @@ estão nos seguintes intervalos: [0-25], [26-50], [51-75] e [76-100]. A entrada 
 terminar quando for lido um número negativo. */
 
 const input = require('readline-sync')
-let numeros = []
-let i = 0
 
-while (i >= 0) {
-    let num = parseInt(input.question("Digite um nº inteiro ou um nº negativo para sair: "))
-    numeros.push(num)
-    if (num < 0) {
-        console.log("Saindo")
+let num0a25 = []
+let num26a50 = []
+let num51a75 = []
+let num75a100 = []
+
+let num = Number(input.question("Digite um nº inteiro ou um nº negativo para sair: "))
+
+if (num < 0) {
+    console.log("saindo")
+}else if (num <= 25) {
+    while (num <= 25) {
+        num = Number(input.question("Digite um nº inteiro ou um nº negativo para sair: "))
+        num0a25.push(num)
+    }
+}console.log(num0a25.length)
+
+/*for (let num = 0; num >= 0;) {
+    let num = Number(input.question("Digite um nº inteiro ou um nº negativo para sair: "))
+    if (num <= 25) {
+        num0a25.push(num)
     }
 }
+
+if (num < 0) {
+    console.log("saindo")
+}
+console.log(num0a25.length)*/
