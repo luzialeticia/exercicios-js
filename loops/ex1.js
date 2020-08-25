12324a110b86a63461f83b20959ee7c950773f0d
luzialeticia <leticialuzia@tutamail.com>
2020-08-25 17:42:46 -0300

    Modificando algoritmo

--- loops/ex1.js
+++ loops/ex1.js
@@ -3,8 +3,8 @@ três e que se encontram no conjunto dos números de 1 até 500. */
 
 let soma = 0
 
-for (let i = 1; i <= 500; i++) {
-    if (i % 2 !== 0 && i % 3 === 0) {
+for (let i = 3; i <= 500; i+3) {
+    if (i % 2 !== 0) {
         soma += i
     }
     console.log(soma)
