98dd00804140b8df01fac6834aa35a409bf2254d
luzialeticia <leticialuzia@tutamail.com>
2020-08-25 18:09:22 -0300

    Escevendo o algoritmo

--- loops/ex10.js
+++ loops/ex10.js
@@ -1,3 +1,11 @@
 /* Escreva um algoritmo que leia um valor inicial A e imprima a seqüência de valores do cálculo de
 A! e o seu resultado. Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120 */
 
+const input = require('readline-sync')
+
+let a = parseInt(input.question("Digite um valor inteiro para calcular seu fatorial: "))
+
+for (let i = 1; i <= a; i++) {
+    let result = i*i
+    console.log(result)
+}
