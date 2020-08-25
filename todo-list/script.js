e75e313a06f1b053539220672c6ac21c1617175e
luzialeticia <leticialuzia@tutamail.com>
2020-08-25 12:40:57 -0300

    Colocando o if else dentro do while

--- todo-list/script.js
+++ todo-list/script.js
@@ -2,9 +2,12 @@ let todos = []
 
 const input = prompt("What would you like to do?")
 
-if(input === "new") {
-    let newTodo = prompt("Enter new todo")
-    todos.push(newTodo)
-} else if(input === "list") {
-    console.log(todos)
+while(input !== "quit") {
+    if(input === "new") {
+        let newTodo = prompt("Enter new todo")
+        todos.push(newTodo)
+    } else if(input === "list") {
+        console.log(todos)
+    }
 }
+console.log("OK, YOU QUIT THE APP!")
