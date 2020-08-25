fb20b067ce4e574e7b845612233c9d084c016544
luzialeticia <leticialuzia@tutamail.com>
2020-08-25 17:53:42 -0300

    Tentando criar o algoritmo com while e if

--- loops/ex4.js
+++ loops/ex4.js
@@ -2,3 +2,14 @@
 estão nos seguintes intervalos: [0-25], [26-50], [51-75] e [76-100]. A entrada de dados deve
 terminar quando for lido um número negativo. */
 
+const input = require('readline-sync')
+let numeros = []
+let i = 0
+
+while (i >= 0) {
+    let num = parseInt(input.question("Digite um nº inteiro ou um nº negativo para sair: "))
+    numeros.push(num)
+    if (num < 0) {
+        console.log("Saindo")
+    }
+}
