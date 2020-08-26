16ceeffdce7f4976dec8ad301eb5560b5686ecaa
luzialeticia <leticialuzia@tutamail.com>
2020-08-26 19:01:44 -0300

    Criando a lógica for com if dentro

--- diversos-reprograma/ex9.js
+++ diversos-reprograma/ex9.js
@@ -1,1 +1,10 @@
 // Faça um algoritmo que imprima a soma dos números múltiplos de 3 e 5, menores que 1000.
+
+let soma = 0
+
+for(let i = 1; i <= 20; i++) {
+    if(i % 3 === 0 && i % 5 === 0){
+        soma += i
+        console.log(soma)
+    }
+}
