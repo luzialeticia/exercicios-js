dcbc8493693a83eede604c6658f9445599f9a18c
luzialeticia <leticialuzia@tutamail.com>
2020-08-25 21:02:47 -0300

    Esboço do exercício 7

---
+++ diversos-reprograma/ex7.js
@@ -0,0 +1,11 @@
+/* Faça um algoritmo que imprima números inteiros de n a 1 (Ex.: O usuário digita o número 10.
+Você deve lista 10, 9, 8, 7, 6, 5, 4, 3, 2, 1); */
+
+const input = require('readline-sync')
+
+let num = parseInt(input.question("Digite um nº inteiro: "))
+
+while (num >= 1) {
+    num = num--
+    console.log(num)
+}
