c7937cc7128617f563711b34c53b88eeeb1d7a7e
luzialeticia <leticialuzia@tutamail.com>
2020-08-27 11:53:30 -0300

    Adicionando 1º exercicio

---
+++ array-loops/print-reverse.js
@@ -0,0 +1,9 @@
+/* Write a function 'printReverse() that takes an array as an argument
+and prints out the elements in the array in reverse orden
+(don't actually reverse the array itself) 
+printReverse([1, 2, 3, 4]):
+4
+3
+2
+1*/
+
