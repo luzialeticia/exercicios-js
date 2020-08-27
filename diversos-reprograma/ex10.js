2a3eab84b19323880c52dfb36c8c0894fbfe5db9
luzialeticia <leticialuzia@tutamail.com>
2020-08-27 15:27:11 -0300

    Declarando as variáveis e criando a função da tabuada

--- diversos-reprograma/ex10.js
+++ diversos-reprograma/ex10.js
@@ -1,2 +1,12 @@
 //Faça um Programa que leia um numero e escreva a tabuada do mesmo de 1 a 10.
 
+function tabuada(numero) {
+    for(let i = 1; i <= 10; i++) {
+        let resultado = i * numero
+        return `${numero} X ${i} = ${resultado}`
+    }
+}
+
+const input = require('readline-sync')
+
+const num = parseInt(input.question("Digite um valor inteiro para ver sua tabuada: "))
