fc43a5b3391c71bf2645bad44339643096e67ea9
luzialeticia <leticialuzia@tutamail.com>
2020-08-26 21:35:15 -0300

    Colocando o console.log fora do for

--- loops/ex1.js
+++ loops/ex1.js
@@ -3,9 +3,9 @@ três e que se encontram no conjunto dos números de 1 até 500. */
 
 let soma = 0
 
-for (let i = 3; i <= 500; i+3) {
-    if (i % 2 !== 0) {
+for (let i = 1; i <= 500; i++) {
+    if (i % 2 !== 0 && i % 3 === 0) {
         soma += i
     }
-    console.log(soma)
 }
+console.log(soma)
