3e96eb9ed58778f6acd1c058df47d070f064c00f
luzialeticia <leticialuzia@tutamail.com>
2020-08-27 12:01:26 -0300

    1ª tentativa de forEach

--- array-loops/print-reverse.js
+++ array-loops/print-reverse.js
@@ -1,5 +1,4 @@
-/* Write a function 'printReverse() that takes an array as an argument
-and prints out the elements in the array in reverse orden
+/* Write a function 'printReverse() that takes an array as an argument and prints out the elements in the array in reverse orden
 (don't actually reverse the array itself) 
 printReverse([1, 2, 3, 4]):
 4
@@ -7,3 +6,11 @@ printReverse([1, 2, 3, 4]):
 2
 1*/
 
+function printReverse(array) {
+    array.forEach((element,index) => {
+        return `${index}: ${element}`
+    });
+}
+
+let nums = [1, 2, 3, 4]
+console.log(printReverse(nums))
