127ecc3ddcbb15813ef98d04c4a8d0b3d8795792
luzialeticia <leticialuzia@tutamail.com>
2020-08-27 11:42:53 -0300

    Refatorando com forEach

--- todo-list/script.js
+++ todo-list/script.js
@@ -4,10 +4,16 @@ let input = prompt("What would you like to do?")
 
 while(input !== "quit") {
     if(input === "list") {
-        console.log(todos)
+        todos.forEach(function (todo, index){
+            console.log(index + todo)
+        })
     } else if(input === "new") {
         let newTodo = prompt("Enter new todo")
         todos.push(newTodo)
+    } else if(input === "delete") {
+        let index = parseInt(prompt("What the Todo's index that you want to delete?"))
+        todos.splice(index, 1)
+        console.log("The Todo has been deleted.")
     }
     input = prompt("What would you like to do?")
 }
