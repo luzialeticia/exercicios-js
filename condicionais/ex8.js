0dc7cd1cf00b794b1948d86e6976df27a508acac
luzialeticia <leticialuzia@tutamail.com>
2020-08-28 22:49:46 -0300

    Tentativa com if else

--- condicionais/ex8.js
+++ condicionais/ex8.js
@@ -1,2 +1,10 @@
 /* 8) Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem
 decrescente. */
+
+function ordem(a, b, c) {
+    if(a > b && a > c) {
+        return `${a}\n${b}\n${c}`
+    }else if(b > a && b > c) {
+        return `${b}\n${b}\n${c}`
+    }
+}
