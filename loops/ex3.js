26da4866855eaff3a2b16642dbdd44582592c40a
luzialeticia <leticialuzia@tutamail.com>
2020-09-14 11:43:28 -0300

    Resolvendo a 2ª parte do ex3

--- loops/ex3.js
+++ loops/ex3.js
@@ -8,16 +8,29 @@ let valores = []
 let valor;
 let soma;
 let media;
+let negativos;
+let positivos;
+let quantNegativos;
+let quantPositivos;
+// let porcentagemNegativos;
+// let porcentagemPositivos;
 
 do {
-    valor = parseInt(input.question("Digite um nº inteiro ou 0 para sair: "))
+    valor = parseInt(input.question("Digite um nº inteiro ou S para sair: "))
     valores.push(valor)
+
+    negativos = valores.filter(item => item < 0)
+    quantNegativos = negativos.length
+
+    positivos = valores.filter(item => item > 0)
+    quantPositivos = positivos.length
 
     soma = valores.reduce((accumulator, currentItem) => {
         return (accumulator + currentItem)
     },0)
-} while (valor > 0)
+} while (valor !== 0)
 
 media = soma/(valores.length-1)
 
-console.log(media)
+
+console.log(media, quantPositivos, quantNegativos)
