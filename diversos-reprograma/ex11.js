82e04d403e0e38054ecca799e50b8cbcc75523b7
luzialeticia <leticialuzia@tutamail.com>
2020-09-14 17:47:52 -0300

    PArcialmenteresolvido. Queria impedir de adicionar o nº negativo no array

--- diversos-reprograma/ex11.js
+++ diversos-reprograma/ex11.js
@@ -1,2 +1,20 @@
 // Faça um programa que leia um array e informe o maior número.
 
+const input = require('readline-sync')
+
+const array = new Array()
+
+let numeros = parseInt(input.question("Digite um nº para colocar no array: "))
+let sortComSlice;
+let maiorNum;
+
+do {
+    numeros = parseInt(input.question("Digite um nº para colocar no array: "))
+    array.push(numeros)
+
+    sortComSlice = array.slice().sort((a, b) => b-a)
+
+    maiorNum = sortComSlice[0]
+} while(numeros >= 0)
+
+console.log(maiorNum);
