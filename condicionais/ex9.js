1711148f8787949c8cf3a7ca22106fbf641e99d6
luzialeticia <leticialuzia@tutamail.com>
2020-09-14 10:32:59 -0300

    Criando o arquivo e resolvendo ex9

--- condicionais/ex9.js
+++ condicionais/ex9.js
@@ -3,6 +3,22 @@ calcule seu peso ideal, utilizando as seguintes fórmulas:
 ● para homens: (72.7 * h) – 58;
 ● para mulheres: (62.1 * h) – 44.7. */
 
-function pesoIdealH (h) {
-    
+const input = require('readline-sync')
+
+const pesoIdeal = (sexo, altura) => {
+    if (sexo === "M" || sexo === "m") {
+        return ((72.7 * altura) - 58).toFixed(1)
+    } else if (sexo === "F" || sexo === "f") {
+        return ((72.7 * altura) - 44.7).toFixed(1)
+    } else {
+        return "Inválido."
+    }
 }
+
+const nome = input.question("Qual o seu nome: ")
+const genero = input.question("Qual seu gênero? (F para feminino ou M para masculino) ")
+const altura = parseFloat(input.question("Digite sua altura: "))
+
+pesoIdeal(genero, altura)
+
+console.log(`${nome}, seu peso ideal é ${pesoIdeal(genero, altura)}kg.`);
