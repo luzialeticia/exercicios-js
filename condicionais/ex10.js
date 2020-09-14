fecee2e40439aca40e1f26790292fa8baec751d3
luzialeticia <leticialuzia@tutamail.com>
2020-09-14 11:00:59 -0300

    Criando o arquivo e resolvendo ex10

---
+++ condicionais/ex10.js
@@ -0,0 +1,34 @@
+/* 10) O IMC – Indice de Massa Corporal é um critério da Organização Mundial de Saúde para dar
+uma indicação sobre a condição de peso de uma pessoa adulta. A fórmula é IMC = peso / ( altura )2
+Elabore um algoritmo que leia o peso e a altura de um adulto e mostre sua condição de acordo
+com a tabela abaixo.
+IMC em adultos Condição
+Abaixo de 18,5 Abaixo do peso
+Entre 18,5 e 25 Peso normal
+Entre 25 e 30 Acima do peso
+Acima de 30 obeso */
+
+const input = require('readline-sync')
+
+const imc = (peso, altura) => {
+    return (peso/Math.pow(altura, 2)).toFixed(1)
+}
+
+const calculaIMC = (funcaoCalculaIMC) => {
+    if (funcaoCalculaIMC < 18.5) {
+        return `Seu IMC é ${funcaoCalculaIMC}. Você está abaixo do peso normal.`
+    } else if (funcaoCalculaIMC >= 18.5 && funcaoCalculaIMC <= 25) {
+        return `Seu IMC é ${funcaoCalculaIMC}. Você está no peso normal.`
+    } else if (funcaoCalculaIMC > 25 && funcaoCalculaIMC <= 30) {
+        return `Seu IMC é ${funcaoCalculaIMC}. Você está acima do peso.`
+    } else {
+        return `Seu IMC é ${funcaoCalculaIMC}. Você está obese.`
+    }
+}
+
+const peso = parseFloat(input.question("Digite o peso: "))
+const altura = parseFloat(input.question("Digite a altura: "))
+
+imc(peso, altura)
+
+console.log(calculaIMC(imc(peso, altura)));
