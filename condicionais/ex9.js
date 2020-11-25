6348607f5ab25a113aea17148785ef9580c6ad26
luzialeticia <leticialuzia@tutamail.com>
2020-11-25 19:03:04 -0300

    Refatorando

--- condicionais/ex9.js
+++ condicionais/ex9.js
@@ -5,20 +5,16 @@ calcule seu peso ideal, utilizando as seguintes fórmulas:
 
 const input = require('readline-sync')
 
-const pesoIdeal = (sexo, altura) => {
-    if (sexo === "M" || sexo === "m") {
-        return ((72.7 * altura) - 58).toFixed(1)
-    } else if (sexo === "F" || sexo === "f") {
-        return ((72.7 * altura) - 44.7).toFixed(1)
-    } else {
-        return "Inválido."
-    }
-}
-
 const nome = input.question("Qual o seu nome: ")
 const genero = input.question("Qual seu gênero? (F para feminino ou M para masculino) ")
 const altura = parseFloat(input.question("Digite sua altura: "))
 
-pesoIdeal(genero, altura)
-
-console.log(`${nome}, seu peso ideal é ${pesoIdeal(genero, altura)}kg.`);
+if(genero === "M" || genero === "m") {
+    const pesoIdeal = ((72.7 * altura) - 58).toFixed(1)
+    console.log(`${nome}, seu peso ideal é ${pesoIdeal}kg.`)
+} else if (genero === "F" || genero === "f") {
+    const pesoIdeal = ((72.7 * altura) - 44.7).toFixed(1)
+    console.log(`${nome}, seu peso ideal é ${pesoIdeal}kg.`)
+} else {
+    console.log("Gênero inválido.")
+}
