d51953ffc620468660debf6a746866737909b4b1
luzialeticia <leticialuzia@tutamail.com>
2020-11-26 17:44:06 -0300

    Criando arquivo para o exercício 12

---
+++ condicionais/ex12.js
@@ -0,0 +1,5 @@
+/* 12) Escreva um algoritmo que leia o número de identificação, as 3 notas obtidas por um aluno nas
+3 verificações e a média dos exercícios que fazem parte da avaliação, e calcule a média de
+aproveitamento, usando a fórmula:
+MA := (nota1 + nota 2 * 2 + nota 3 * 3 + ME)/7 */
+
